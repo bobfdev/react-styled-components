da5e3ecada2982e7f4c10816697f9b9edf895930
Bob F <bobfdev@gmail.com>
2021-11-23 16:56:00 -0600

    updated initial files and added images and components folders

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyledCard } from './styles/Card.styled';
+
+export default function Card({item: { id, body, title, image }}) {
+    return (
+        <StyledCard>
+            <div>
+                <h2>{title}</h2>
+                <p>{body}</p>
+            </div>
+            <div>
+                <img src={`./images/${image}`} alt="" />
+            </div>
+        </StyledCard>
+    )
+}
